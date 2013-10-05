b1c6bec0a7b703234e9833674ae4dafd6bb4267e
adinuca <adina_m3dy@yahoo.com>
2013-10-05 16:25:54 +0300

    update task.xml

--- taskManager/src/main/resources/taskmanager_task.sql
+++ taskManager/src/main/resources/taskmanager_task.sql
@@ -1,29 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `taskmanager` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `taskmanager`;
--- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
---
--- Host: localhost    Database: taskmanager
--- ------------------------------------------------------
--- Server version	5.5.19-log
+delimiter $$
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE `taskmanager` /*!40100 DEFAULT CHARACTER SET latin1 */$$
 
---
--- Table structure for table `task`
---
+delimiter $$
 
-DROP TABLE IF EXISTS `task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task` (
   `idTask` int(11) NOT NULL AUTO_INCREMENT,
   `generatedId` varchar(45) NOT NULL,
@@ -34,33 +14,10 @@ CREATE TABLE `task` (
   `timespent` varchar(45) DEFAULT NULL,
   `url` varchar(145) DEFAULT NULL,
   `status` varchar(15) NOT NULL,
-  `idUser` int(11) NOT NULL,
+  `userEmail` varchar(40) NOT NULL,
   `parentTaskId` int(11) DEFAULT NULL,
   PRIMARY KEY (`idTask`),
   UNIQUE KEY `idTask_UNIQUE` (`idTask`),
-  UNIQUE KEY `generatedId_UNIQUE` (`generatedId`),
-  KEY `user_id_fk` (`idUser`),
-  CONSTRAINT `user_id_fk` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE KEY `generatedId_UNIQUE` (`generatedId`)
+) ENGINE=InnoDB AUTO_INCREMENT=251 DEFAULT CHARSET=latin1$$
 
---
--- Dumping data for table `task`
---
-
-LOCK TABLES `task` WRITE;
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` VALUES (33,'Task152','Some bla bla 2','aaa bb cc','2013-01-12','Work','3','ft.com','Defined',10,NULL),(38,'Task453','New123','bla bla bla1','2013-01-12','Work','23','ft.com','defined',10,33),(120,'Task143','VVladi','dsd','2013-03-24','Work','0','s','To do',10,38);
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-03-26 15:50:13
